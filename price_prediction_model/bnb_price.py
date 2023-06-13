ce5f886d02c3192a88949513a224ffa5e970480e
Yassine Ben Chaouat <yassbch@gmail.com>
2023-06-13 15:10:50 +0200

    MODELOS PREPARADOS

---
+++ price_prediction_model/bnb_price.py
@@ -0,0 +1,163 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+from tensorflow.keras.layers import Bidirectional, Dropout, Activation, Dense, LSTM
+from tensorflow.python.keras.layers import CuDNNLSTM
+from tensorflow.keras.models import Sequential
+
+
+url = "/home/yass/code/yass2803/cryptoteller/data/BNBUSDT.csv"
+df = pd.read_csv(url)
+df.head()
+
+
+# Change the time unit of the "open_time" and "close_time" columns
+df["open_time"] = pd.to_datetime(df["open_time"], unit='ms', origin='unix')
+df = df.drop(columns=["close_time"])
+
+
+#Plot the Historical Price
+ax = df.plot(x='open_time', y='close');
+ax.set_xlabel("Date")
+ax.set_ylabel("Close Price (USD)")
+plt.show()
+
+
+
+# Normalization
+scaler = MinMaxScaler()
+
+close_price = df.close.values.reshape(-1, 1)
+
+scaled_close = scaler.fit_transform(close_price)
+
+
+# Preprocessing
+class Preprocessor:
+    def __init__(self, SEQ_LEN=100):
+        self.SEQ_LEN=SEQ_LEN
+        pass
+    def to_sequences(self, data, seq_len):
+        d = []
+
+        for index in range(len(data) - seq_len):
+            d.append(data[index: index + seq_len])
+
+        return np.array(d)
+
+    def preprocess(self, data_raw, train_split):
+
+        data = self.to_sequences(data_raw, self.SEQ_LEN)
+
+        self.num_train = int(train_split * data.shape[0])
+
+        X_train = data[:self.num_train, :-1, :]
+        y_train = data[:self.num_train, -1, :]
+
+        X_test = data[self.num_train:, :-1, :]
+        y_test = data[self.num_train:, -1, :]
+
+        return X_train, y_train, X_test, y_test
+
+preprocessor=Preprocessor()
+X_train, y_train, X_test, y_test = preprocessor.preprocess(scaled_close, train_split = 0.80)
+
+
+# Model
+
+DROPOUT = 0.2
+WINDOW_SIZE = preprocessor.SEQ_LEN - 1
+
+model = keras.Sequential()
+
+model.add(LSTM(WINDOW_SIZE, return_sequences=True, input_shape=(WINDOW_SIZE, X_train.shape[-1])),
+                        )
+model.add(Dropout(rate=DROPOUT))
+
+model.add(LSTM(WINDOW_SIZE * 2, return_sequences=True))
+model.add(Dropout(rate=DROPOUT))
+
+model.add(LSTM(WINDOW_SIZE, return_sequences=False))
+
+model.add(Dense(units=1))
+
+model.add(Activation('linear'))
+
+
+# Training
+
+
+from tensorflow import keras
+from keras.callbacks import EarlyStopping
+
+es = EarlyStopping(monitor="val_loss",
+                       patience=3,
+                       mode="min",
+                       restore_best_weights=True)
+
+model.compile(
+    loss='mean_squared_error',
+    metrics=["mae"] ,
+    optimizer='adam'
+)
+
+
+
+BATCH_SIZE = 64
+
+history = model.fit(
+    X_train,
+    y_train,
+    epochs=6,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    validation_split=0.1,
+    callbacks=[es]
+)
+
+
+
+
+MAE=scaler.inverse_transform([[model.evaluate(X_test, y_test)[1]]])[0][0]
+print(f"MAE for the test set: {MAE} Dollars")
+
+
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+
+#Prediction Plot
+# Prediction
+y_hat = model.predict(X_test)
+
+y_test_inverse = scaler.inverse_transform(y_test)
+y_hat_inverse = scaler.inverse_transform(y_hat)
+
+date_time_test = df.iloc[preprocessor.num_train + preprocessor.SEQ_LEN:, 0].to_numpy()
+
+# Plot the actual and predicted prices
+plt.figure(figsize=(12, 8))  # Set the figure size to make it larger
+plt.plot(date_time_test, y_test_inverse, label="Actual Price", color='green')
+plt.plot(date_time_test, y_hat_inverse, label="Predicted Price", color='red')
+
+# Customize the plot
+plt.title('BNB Price Prediction')
+plt.xlabel('Time [days]')
+plt.ylabel('Price')
+plt.xticks(rotation=45)  # Rotate x-axis labels by 45 degrees for better readability
+plt.legend(loc='best')
+
+plt.show()
---
+++ project_crypto_models_vs_sentiment/bnb__vs_sentiment.py
@@ -0,0 +1,206 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Bidirectional, Dropout, Activation, Dense, LSTM
+from tensorflow.python.keras.layers import CuDNNLSTM
+from tensorflow.keras.models import Sequential, Model
+import random
+import sklearn
+from tensorflow.keras.regularizers import L1L2
+
+
+# Read the data from a CSV file
+url = "/home/yass/code/yass2803/cryptoteller/data/BNBUSDT.csv"
+df = pd.read_csv(url)
+df.head()
+
+# Change the time unit of the "open_time" column
+df["open_time"] = pd.to_datetime(df["open_time"], unit='ms', origin='unix')
+df = df.drop(columns=["close_time"])
+
+df.shape
+
+# Plot the closing price over time
+ax = df.plot(x='open_time', y='close');
+ax.set_xlabel("Date")
+ax.set_ylabel("Close Price (USD)")
+
+plt.show()
+
+# Normalization
+scaler = MinMaxScaler()
+close_price = df.close.values.reshape(-1, 1)
+scaled_close = scaler.fit_transform(close_price)
+
+scaled_close.shape
+
+np.isnan(scaled_close).any()
+
+
+def date_to_string(date_):
+    year = str(date_.year)
+    month = str(date_.month)
+    day = str(date_.day)
+
+    if len(day) == 1:
+        day = "0" + day
+
+    if len(month) == 1:
+        month = "0" + month
+
+    return year + "-" + month + "-" + day
+
+df_sentiment = pd.read_csv("/home/yass/code/yass2803/cryptoteller/data/sentiment/aggr_sent_bnb.csv")
+
+df_dates = df[["open_time"]].copy()
+df_dates.loc[:,"date"] = df_dates.open_time.apply(lambda x: date_to_string(x))
+
+df_sentiment = df_dates.merge(df_sentiment, on="date", how="left")
+df_sentiment.drop(columns="date", inplace=True)
+df_sentiment.head()
+
+
+not_nan_idx = np.where(~np.isnan(df_sentiment.sentiment.values))[0]
+
+sentiment = df_sentiment.iloc[not_nan_idx,:].sentiment.values.reshape(-1, 1)
+
+scaled_returns_red = scaled_close[not_nan_idx]
+
+OHE_encoder=sklearn.preprocessing.OneHotEncoder()
+OHE_sentiment=OHE_encoder.fit_transform(sentiment).toarray()
+
+n_sentiment_classes = len(OHE_encoder.categories_[0])
+
+# Preprocessing
+class Preprocessor:
+    def __init__(self, SEQ_LEN=120):
+        self.SEQ_LEN=SEQ_LEN
+        pass
+    def to_sequences(self, data,sentiment, seq_len):
+        d = []
+        s=[]
+
+        for index in range(len(data) - seq_len):
+            d.append(data[index: index + seq_len])
+            s.append(sentiment[index:index+seq_len])
+
+        return np.array(d), np.array(s)
+
+    def preprocess(self, data_raw,sentiment, train_split):
+
+        data, sentiment = self.to_sequences(data_raw, sentiment, self.SEQ_LEN)
+
+        self.num_train = int(train_split * data.shape[0])
+
+        X_train = data[:self.num_train, :-1, :]
+        y_train = data[:self.num_train, -1, :]
+        sentiment_train=sentiment[:self.num_train, :-1, :]
+
+        X_test = data[self.num_train:, :-1, :]
+        y_test = data[self.num_train:, -1, :]
+        sentiment_test=sentiment[self.num_train:, :-1, :]
+
+        return X_train, y_train, X_test, y_test, sentiment_train, sentiment_test
+
+preprocessor=Preprocessor()
+X_train, y_train, X_test, y_test, sentiment_train, sentiment_test = preprocessor.preprocess(scaled_returns_red,OHE_sentiment, train_split = 0.80)
+
+print(f"sentiment train dtype: {sentiment_train.dtype}")
+
+X_train.shape
+X_test.shape
+
+# Model
+
+DROPOUT = 0.2
+WINDOW_SIZE = preprocessor.SEQ_LEN - 1
+
+
+
+def init_model(WINDOW_SIZE, n_sentiments, dropout, loss, optimizer, metrics):
+
+    inputs_price = keras.Input(shape=(WINDOW_SIZE, 1))
+    inputs_sentiment = keras.Input(shape=(WINDOW_SIZE,n_sentiments))
+    hidden_lstm=LSTM(10, return_sequences=False, kernel_regularizer=L1L2(l1=0.05, l2=0.05))(inputs_price)
+
+    concat = keras.layers.concatenate([hidden_lstm, inputs_sentiment[:, -1, :]], -1)
+    hidden_final=keras.layers.Dense(32)(concat)
+    output=keras.layers.Dense(1)(hidden_final)
+    model = Model(inputs=[inputs_price, inputs_sentiment], outputs=output)
+
+    print(model.summary())
+    return model
+
+
+
+
+model=init_model(WINDOW_SIZE, n_sentiment_classes, DROPOUT, 'mean_squared_error', 'adam', ["mae"])
+# Training
+from tensorflow import keras
+from keras.callbacks import EarlyStopping
+from tensorflow.keras import optimizers
+
+es = EarlyStopping(monitor="val_loss",
+                       patience=15,
+                       mode="min",
+                       restore_best_weights=True)
+
+
+model.compile(
+    loss='mean_squared_error',
+    metrics=["mae"] ,
+    optimizer="adam"
+)
+
+BATCH_SIZE = 64
+
+history = model.fit(
+    [X_train, sentiment_train],
+    y_train,
+    epochs=20,
+    batch_size=BATCH_SIZE,
+    shuffle=False,
+    validation_split=0.1
+)
+
+
+# Evaluation
+MAE=scaler.inverse_transform([[model.evaluate([X_test, sentiment_test], y_test)[1]]])[0][0]
+print(f"MAE for the test set: {MAE}")
+
+# Plot the training loss and validation loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Prediction
+y_hat = model.predict([X_test, sentiment_test])
+# Inverse transform the scaled data to get the actual prices
+y_test_inverse = scaler.inverse_transform(y_test)
+y_hat_inverse = scaler.inverse_transform(y_hat)
+
+# Plot the actual and predicted prices
+# Prediction
+date_time_test = df.iloc[not_nan_idx,:].iloc[preprocessor.num_train + preprocessor.SEQ_LEN:, 0].to_numpy()
+
+# Plot the actual and predicted prices
+plt.figure(figsize=(12, 8))  # Set the figure size to make it larger
+plt.plot(date_time_test, y_test_inverse, label="Actual Price", color='green')
+plt.plot(date_time_test, y_hat_inverse, label="Predicted Price", color='red')
+
+# Customize the plot
+plt.title('BNB Price Prediction')
+plt.xlabel('Time [days]')
+plt.ylabel('Price')
+plt.show()
